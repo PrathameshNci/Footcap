66dc425b2c7b80923988c77820e1db266afd3193
PrathameshNci <157479341+PrathameshNci@users.noreply.github.com>
2024-04-22 23:04:17 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["18.201.92.40"]/' /home/ubuntu/shoecom/shoecom/settings.py
+sed -i 's/\[]/\["3.249.234.213"]/' /home/ubuntu/shoecom/shoecom/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
