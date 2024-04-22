c728088323f242f59bb4604fd2442d44583c731e
PrathameshNci <157479341+PrathameshNci@users.noreply.github.com>
2024-04-22 23:57:33 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,3 +3,5 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/shoecom/requirements.txt
+sudo chmod 777 /home/ubuntu/clothing
+sudo chmod 777 /home/ubuntu/clothing/db.sqlite3
