524008fc9022df9c378c995bfdb247867b372b22
PrathameshNci <157479341+PrathameshNci@users.noreply.github.com>
2024-04-23 18:35:02 +0530

    Add files via upload

---
+++ packageparth/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="packagepart",
+      version="0.1",
+      description="Parth Package",
+      author="Parth",
+      packages=['packageparth'],
+      install_requires=['datetime', 'random2'])
